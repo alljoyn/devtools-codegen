89d7153daaece40cd58d96fcadff31752cb374e2
Joseph Huffman <jhuffman@qce.qualcomm.com>
2014-06-10 15:11:32 -0700

    Added self compile of Cheetah .tmpl files upon first execution.
    Change-Id: I04b759069267f46088358b7521d00f729664e834
    Signed-off-by: Joseph Huffman <jhuffman@qce.qualcomm.com>

--- scripts/ajcodegen.py
+++ scripts/ajcodegen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2013 AllSeen Alliance. All rights reserved.
+# Copyright (c) 2013-2014 AllSeen Alliance. All rights reserved.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,5 +14,82 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #!Python script for invoking the AllJoyn code generator.
+import os
+import sys
+import fnmatch
+import subprocess
+import platform
+
+def __get_codegen_package_path():
+    scripts_dir = os.path.dirname(sys.argv[0])
+    base_dir = os.path.dirname(scripts_dir)
+    codegen_sub_directory = "Lib\site-packages\AllJoynCodeGen"
+    codegen_complete_directory = os.path.join(base_dir, codegen_sub_directory)
+
+    return codegen_complete_directory
+
+def __get_cheetah_command():
+    scripts_dir = os.path.dirname(sys.argv[0])
+    cheetah_py = os.path.join(scripts_dir, "cheetah.py")
+    if not os.path.isfile(cheetah_py):
+        print("'{0}' was not found.".format(cheetah_py))
+        return []
+
+    return_value = []
+
+    system_type = platform.system()
+
+    if system_type == "Linux" or system_type == "Darwin":
+        return_value.append("python")
+    elif system_type == "Windows":
+        return_value.append("python.exe")
+    else:
+        print("Unrecognized system type '{0}'".format(system_type))
+        return []
+
+    return_value.extend([cheetah_py, "compile"]);
+
+    return return_value
+
+def __compile_template_files_if_needed():
+    path = __get_codegen_package_path()
+
+    # Make sure this is a valid directory
+    if not os.path.isdir(path):
+        print("Code generator directory '{0}' was not found!".format(path))
+        return
+
+    src_files = []
+
+    for dirpath, dirs, files in os.walk(path):
+        # The "src" portion of the existing directory is not used in the destination path.
+        base = dirpath.replace("src/", "", 1)
+        dest = os.path.join("Lib/site-packages/AllJoynCodeGen", base)
+
+        for f in files:
+            src = os.path.join(dirpath, f)
+            if fnmatch.fnmatch(f, "*.tmpl"):
+                python_file = "{0}.py".format(os.path.splitext(src)[0])
+                already_compiled = os.path.exists(python_file)
+
+                if already_compiled:
+                    template_file_time = os.path.getmtime(src)
+                    python_file_time = os.path.getmtime(python_file)
+
+                    if template_file_time > python_file_time:
+                        src_files.append(src)
+                else:
+                    src_files.append(src)
+
+    if len(src_files) > 0:
+        cheetah_command = __get_cheetah_command()
+
+        if len(cheetah_command) > 0:
+            cheetah_command.extend(src_files)
+            subprocess.check_output(cheetah_command)
+
+__compile_template_files_if_needed()
+
 from AllJoynCodeGen.codegen import main
+
 main()
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 AllSeen Alliance. All rights reserved.
+# Copyright (c) 2013-2014 AllSeen Alliance. All rights reserved.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,13 +14,72 @@
 
 from distutils.core import setup
 from src import config
+import os
+import fnmatch
+import string
+
+#######
+# template_directories is the list of directories that contain .tmpl files to
+# be compiled with Cheetah into .py files the first time the application is run.
+# It is assumed that, by convention, each of these directories contains
+# the Python file __init__.py and a file named GenXXX.py where 'XXX' is
+# the last subdirectory of each of the template directories. Hence
+# "src/android" contains a GenAndroid.py file and "src/tl" contains a GenTL.py file.
+#
+# All .tmpl files in the named directories will be part of the installation.
+#
+# All files in directories below these directories, not just .tmpl files,
+# will be part of the installation.
+#######
+template_directories = ["src/android",
+                        "src/tl"]
+
+def __create_data_file_collection():
+    return_value = []
+
+    for path in template_directories:
+        # Make sure this is a valid directory
+        if not os.path.isdir(path):
+            print("template directory '{0}' was not found!".format(path))
+            continue
+
+        print("Including template directory '{0}'".format(path))
+        basename = os.path.basename(path)
+        gen_file = "gen{0}.py".format(basename.lower())
+
+        for dirpath, dirs, files in os.walk(path):
+            # The "src" portion of the existing directory is not used in the destination path.
+            base = dirpath.replace("src/", "", 1)
+            dest = os.path.join("Lib/site-packages/AllJoynCodeGen", base)
+            src_files = []
+
+            for f in files:
+                src = os.path.join(dirpath, f)
+                # If this is the root of a template directory grab the GenXXX.py file.
+                if dirpath == path:
+                    if f.lower() == gen_file or f == "__init__.py":
+                        src_files.append(src)
+                    elif fnmatch.fnmatch(f, "*.tmpl"):
+                        src_files.append(src)
+                else:
+                    src_files.append(src)
+
+            dest_src = (dest, src_files)
+            return_value.append(dest_src)
+
+    return return_value
+
+#####
+# This is the collection of 'data' files to be added to the installation beyond
+# those in the "src" directory.
+#####
+data_file_collection = __create_data_file_collection()
 
 setup(name='AllJoynCodeGenSetup',
       version=config.get_version(),
       description='AllJoyn Code Generator',
-      url='http://www.alljoyn.org',
+      url='http://www.alljoyn.org/',
       package_dir={'AllJoynCodeGen': 'src'},
-      packages=['AllJoynCodeGen', 'AllJoynCodeGen.tl'],
-      scripts=['scripts/ajcodegen.py']
-          )
-
+      packages=['AllJoynCodeGen'],
+      scripts=['scripts/ajcodegen.py'],
+      data_files=data_file_collection)

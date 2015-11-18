6061c0862c55c23a2ba9fd77df5936f2a3c9d584
Mathew Martineau <mathewm@qce.qualcomm.com>
2015-11-18 09:55:46 -0800

    Update version number for 15.09
    Change-Id: I3360b7b4bc590c01a304463d6db01ed19f117b19
    Signed-off-by: Mathew Martineau <mathewm@qce.qualcomm.com>

--- src/config.py
+++ src/config.py
@@ -21,7 +21,7 @@ class ConfigException(Exception):
     """Configuration exceptions"""
 
 def get_version():
-    return "0.0.1"
+    return "15.09"
 
 class Config:
     """Contains the configuration obtained from the command line.
